ca0054b42ab44c065b1d001c06c38e45c5eab893
Ryosuke5907 <ydmy20211029@qq.com>
2026-09-23 00:53:09 -0400

    complete lab 01

--- src/lab_68662FBB.c
+++ src/lab_68662FBB.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
-void printscreen()
-{
-    // Please write your code here
+ void printscreen()
+ {
+printf(" #### \n");
+printf("#    #\n"); 
+printf("#    #\n");
+printf("######\n");
+printf("#    #\n");
+printf("#    #\n");
+printf("#    #\n");
 }
 
-// DO NOT change the code below
 #ifndef ___TEST___
 int main()
 {
     printscreen();
     return(0);
 }
+
 #endif
